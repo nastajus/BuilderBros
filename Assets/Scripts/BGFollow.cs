ab84f941533acd00731e78ca15144c8e36e1927e
FamicomPunch <meholzapfel@gmail.com>
2014-04-27 17:52:44 -0400

    0.9a - To fix: swapping players, jumping, CORNERS

---
+++ Assets/Scripts/BGFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGFollow : MonoBehaviour {
+	
+	public Transform player;
+
+	void Update () {
+		transform.position = new Vector3(player.position.x + 6, 4, 0);
+	}
+}
